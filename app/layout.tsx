b32b517c1fe9b6f4f6b8ec85362da1488a902a81
Deependra Chauhan <chauhandeependra2001@gmail.com>
2026-04-16 11:56:38 +0530

    flow without discrepancy complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import SessionActivityTracker from "@/utils/activityTracker";
 
 const inter = Inter({
   variable: '--font-sans',
@@ -27,7 +26,6 @@ export default function RootLayout({
       className={cn("h-full", "antialiased", inter.className)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-      <SessionActivityTracker />
     </html>
   );
 }
