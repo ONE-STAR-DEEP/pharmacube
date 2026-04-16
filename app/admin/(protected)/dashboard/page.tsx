72bd7163a7cb075e3a9386f51a35883f4041f44e
Deependra Chauhan <chauhandeependra2001@gmail.com>
2026-04-16 12:25:58 +0530

    flow without discrepancy complete

--- app/admin/(protected)/dashboard/page.tsx
+++ app/admin/(protected)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='text-center my-auto text-7xl font-extrabold pb-20'>Dashboard</div>
   )
 }
 
