6d6fd8cd0ffdce28a87c05c926edc8213d976885
Deependra Chauhan <chauhandeependra2001@gmail.com>
2026-05-21 17:06:01 +0530

    Fixes

--- components/delivery/sidebar.tsx
+++ components/delivery/sidebar.tsx
@@ -1,4 +1,29 @@
 "use client"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+
+import { FormEvent, useState } from 'react'
+import { Button } from '../ui/button'
+import { Field, FieldGroup, FieldLabel } from "../ui/field"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import { changePassword } from "@/lib/actions/users"
 
 import {
     Sidebar,
@@ -10,20 +35,44 @@ import {
     SidebarMenuItem,
     useSidebar,
 } from "@/components/ui/sidebar"
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
 import Link from "next/link";
-import { FileText, LogOut, LayoutDashboard } from "lucide-react";
+import { FileText, LogOut, LayoutDashboard, Key } from "lucide-react";
 import Image from "next/image";
 import { logout } from "@/lib/logout";
 
 export function AppSidebar() {
 
     const { toggleSidebar } = useSidebar();
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [msg, setMsg] = useState("")
+    const [alertBox, setAlertBox] = useState(false)
+    const [alertTitle, setAlertTitle] = useState("")
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        if (loading) return;
+        setLoading(true);
+        try {
+            const res = await changePassword(currentPassword, newPassword);
+            if (!res.success) {
+                setMsg(res.message)
+                setAlertBox(true)
+                setAlertTitle("Failed to Change Password!!!")
+                return
+            }
+            setOpen(false)
+            setMsg(res.message)
+            setAlertBox(true)
+            setAlertTitle("Alert!!!")
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <Sidebar
@@ -75,6 +124,17 @@ export function AppSidebar() {
                             </Link>
                         </SidebarMenuItem>
 
+                        {/* Change Password */}
+                        <SidebarMenuItem>
+                            <button
+                                onClick={() => { setOpen(true) }}
+                                className="w-full flex items-center gap-2 px-3 py-2 rounded-md hover:bg-muted transition text-left"
+                            >
+                                <Key size={18} />
+                                <span>Change Password</span>
+                            </button>
+                        </SidebarMenuItem>
+
                         {/* Logout */}
                         <SidebarMenuItem>
                             <button
@@ -88,6 +148,79 @@ export function AppSidebar() {
 
                     </SidebarMenu>
                 </SidebarGroup>
+
+                <div>
+                    <Dialog open={open} onOpenChange={setOpen}>
+                        <DialogContent
+                            className="
+                            sm:max-w-md
+                            "
+                        >
+                            <form onSubmit={handleSubmit}>
+                                <DialogHeader className="">
+                                    <DialogTitle className="text-xl">Change Password</DialogTitle>
+                                    <DialogDescription>
+                                        Fill both of the Fields to change Password.
+                                    </DialogDescription>
+                                </DialogHeader>
+
+                                <div className="">
+
+                                    <FieldLabel className="text-lg mt-2 text-primary">
+
+                                    </FieldLabel>
+                                    <FieldGroup className="mt-4">
+
+                                        <Field className="gap-0">
+                                            <div className="flex ">
+                                                <Label htmlFor="currentPassword" className="">Current Password</Label>
+                                                <span className="text-red-500 ">*</span>
+                                            </div>
+                                            <Input id="currentPassword" name="currentPassword" placeholder="Current Password" required
+                                                className="h-10"
+                                                onChange={(e) => setCurrentPassword(e.target.value)}
+                                            />
+                                        </Field>
+
+                                        <Field className="gap-0">
+                                            <div className="flex ">
+                                                <Label htmlFor="newPassword" className="">New Password</Label>
+                                                <span className="text-red-500 ">*</span>
+                                            </div>
+                                            <Input id="newPassword" name="newPassword" placeholder="New Password" required
+                                                className="h-10"
+                                                onChange={(e) => setNewPassword(e.target.value)}
+                                            />
+                                        </Field>
+
+                                    </FieldGroup>
+                                </div>
+
+                                <div className="p-2 pt-4 flex justify-end gap-3">
+                                    <DialogClose asChild>
+                                        <Button variant="outline" type="button">Cancel</Button>
+                                    </DialogClose>
+                                    <Button type="submit" disabled={loading}>Submit</Button>
+                                </div>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+
+                    <AlertDialog open={alertBox} onOpenChange={setAlertBox}>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>{alertTitle}</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    {msg}
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogAction>Close</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+
+                </div>
 
             </SidebarContent>
 
--- components/reviewer/sidebar.tsx
+++ components/reviewer/sidebar.tsx
@@ -1,4 +1,29 @@
 "use client"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+
+import { FormEvent, useState } from 'react'
+import { Button } from '../ui/button'
+import { Field, FieldGroup, FieldLabel } from "../ui/field"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import { changePassword } from "@/lib/actions/users"
 
 import {
     Sidebar,
@@ -10,11 +35,43 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Link from "next/link";
-import { Users, FileText, LogOut, Home, LayoutDashboard, FileX } from "lucide-react";
+import { Users, FileText, LogOut, Home, LayoutDashboard, FileX, Key } from "lucide-react";
 import Image from "next/image";
 import { logout } from "@/lib/logout";
 
 export function AppSidebar() {
+
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [msg, setMsg] = useState("")
+    const [alertBox, setAlertBox] = useState(false)
+    const [alertTitle, setAlertTitle] = useState("")
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        if (loading) return;
+        setLoading(true);
+        try {
+            const res = await changePassword(currentPassword, newPassword);
+            if (!res.success) {
+                setMsg(res.message)
+                setAlertBox(true)
+                setAlertTitle("Failed to Change Password!!!")
+                return
+            }
+            setOpen(false)
+            setMsg(res.message)
+            setAlertBox(true)
+            setAlertTitle("Alert!!!")
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
 
         <Sidebar
@@ -52,6 +109,7 @@ export function AppSidebar() {
                                 <span>Dashboard</span>
                             </Link>
                         </SidebarMenuItem>
+
                         {/* Invoices */}
                         <SidebarMenuItem>
                             <Link
@@ -73,6 +131,17 @@ export function AppSidebar() {
                             </Link>
                         </SidebarMenuItem>
 
+                        {/* Change Password */}
+                        <SidebarMenuItem>
+                            <button
+                                onClick={() => { setOpen(true) }}
+                                className="w-full flex items-center gap-2 px-3 py-2 rounded-md hover:bg-muted transition text-left"
+                            >
+                                <Key size={18} />
+                                <span>Change Password</span>
+                            </button>
+                        </SidebarMenuItem>
+
                         {/* Logout */}
                         <SidebarMenuItem>
                             <button
@@ -86,6 +155,79 @@ export function AppSidebar() {
 
                     </SidebarMenu>
                 </SidebarGroup>
+
+                <div>
+                    <Dialog open={open} onOpenChange={setOpen}>
+                        <DialogContent
+                            className="
+                            sm:max-w-md
+                            "
+                        >
+                            <form onSubmit={handleSubmit}>
+                                <DialogHeader className="">
+                                    <DialogTitle className="text-xl">Change Password</DialogTitle>
+                                    <DialogDescription>
+                                        Fill both of the Fields to change Password.
+                                    </DialogDescription>
+                                </DialogHeader>
+
+                                <div className="">
+
+                                    <FieldLabel className="text-lg mt-2 text-primary">
+
+                                    </FieldLabel>
+                                    <FieldGroup className="mt-4">
+
+                                        <Field className="gap-0">
+                                            <div className="flex ">
+                                                <Label htmlFor="currentPassword" className="">Current Password</Label>
+                                                <span className="text-red-500 ">*</span>
+                                            </div>
+                                            <Input id="currentPassword" name="currentPassword" placeholder="Current Password" required
+                                                className="h-10"
+                                                onChange={(e) => setCurrentPassword(e.target.value)}
+                                            />
+                                        </Field>
+
+                                        <Field className="gap-0">
+                                            <div className="flex ">
+                                                <Label htmlFor="newPassword" className="">New Password</Label>
+                                                <span className="text-red-500 ">*</span>
+                                            </div>
+                                            <Input id="newPassword" name="newPassword" placeholder="New Password" required
+                                                className="h-10"
+                                                onChange={(e) => setNewPassword(e.target.value)}
+                                            />
+                                        </Field>
+
+                                    </FieldGroup>
+                                </div>
+
+                                <div className="p-2 pt-4 flex justify-end gap-3">
+                                    <DialogClose asChild>
+                                        <Button variant="outline" type="button">Cancel</Button>
+                                    </DialogClose>
+                                    <Button type="submit" disabled={loading}>Submit</Button>
+                                </div>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+
+                    <AlertDialog open={alertBox} onOpenChange={setAlertBox}>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>{alertTitle}</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    {msg}
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogAction>Close</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+
+                </div>
 
             </SidebarContent>
 
